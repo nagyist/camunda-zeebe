43a53b9d7fd0ae6f5449aaa3b9344c9b0c18de28
Carlo Sana <carlo.sana@camunda.com>
2026-01-29 13:42:56 +0100

    refactor: BufferWriter.write returns the written length

--- zeebe/protocol-impl/src/main/java/io/camunda/zeebe/protocol/impl/record/RecordMetadata.java
+++ zeebe/protocol-impl/src/main/java/io/camunda/zeebe/protocol/impl/record/RecordMetadata.java
@@ -143,7 +143,8 @@ public final class RecordMetadata implements BufferWriter, BufferReader {
   }
 
   @Override
-  public void write(final MutableDirectBuffer buffer, int offset) {
+  public int write(final MutableDirectBuffer buffer, int offset) {
+    final int initialOffset = offset;
     headerEncoder.wrap(buffer, offset);
 
     headerEncoder
