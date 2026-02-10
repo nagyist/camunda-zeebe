camunda/camunda

feat: expose configuration options in the unified config

zeebe/broker/src/main/java/io/camunda/zeebe/broker/partitioning/startup/RaftPartitionFactory.java
@@ -108,6 +108,12 @@ public final class RaftPartitionFactory {
     partitionConfig.setPreferSnapshotReplicationThreshold(
         brokerCfg.getExperimental().getRaft().getPreferSnapshotReplicationThreshold());
 
+    partitionConfig.setEngineName(brokerCfg.getExperimental().getDefaultEngineName());
+    partitionConfig.setEnableLegacySender(
+        brokerCfg.getExperimental().isEnableLegacySenderSubjects());
+    partitionConfig.setEnableLegacyReceiver(
+        brokerCfg.getExperimental().isEnableLegacyReceiverSubjects());
+
     return new RaftPartition(
         partitionMetadata, partitionConfig, partitionDirectory.toFile(), partitionMeterRegistry);
   }

nagyist/camunda-zeebe

zeebe/broker/src/main/java/io/camunda/zeebe/broker/partitioning/startup/RaftPartitionFactory.java
@@ -114,6 +114,12 @@ public final class RaftPartitionFactory {
     partitionConfig.setPreferSnapshotReplicationThreshold(
         brokerCfg.getExperimental().getRaft().getPreferSnapshotReplicationThreshold());
 
+    partitionConfig.setEngineName(brokerCfg.getExperimental().getDefaultEngineName());
+    partitionConfig.setLegacySenderSubjectsDisabled(
+        brokerCfg.getExperimental().isLegacySenderSubjectsDisabled());
+    partitionConfig.setLegacyReceiverSubjectsDisabled(
+        brokerCfg.getExperimental().isLegacyReceiverSubjectsDisabled());
+
     return new RaftPartition(
         partitionMetadata, partitionConfig, partitionDirectory.toFile(), partitionMeterRegistry);
   }