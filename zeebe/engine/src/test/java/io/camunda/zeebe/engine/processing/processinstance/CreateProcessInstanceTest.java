1df926cbc78495cd62f1f365adcc64d13fb2ae7d
remco.westerhoud <remcowesterhoud@msn.com>
2026-02-10 17:20:59 +0100

    test: enforce short-circuiting on RecordingExporter assertions
    I had to change the return type of expectNoMatchingRecords to T, as sometime it would be desirable to return a list,
    or a boolean instead of a record stream.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/processinstance/CreateProcessInstanceTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/processinstance/CreateProcessInstanceTest.java
@@ -224,12 +224,7 @@ public final class CreateProcessInstanceTest {
     // given
     ENGINE
         .deployment()
-        .withXmlResource(
-            Bpmn.createExecutableProcess("process")
-                .startEvent()
-                .serviceTask("my-task", t -> t.zeebeJobType("my-job"))
-                .endEvent()
-                .done())
+        .withXmlResource(Bpmn.createExecutableProcess("process").startEvent().endEvent().done())
         .deploy();
 
     // when
