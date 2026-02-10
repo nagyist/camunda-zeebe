wasup-yash/camunda

feat: generate a new chunk key in `BatchOperationCreateChunkProcessor`

zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
@@ -123,7 +123,7 @@ public final class BatchOperationSetupProcessors {
         .onCommand(
             ValueType.BATCH_OPERATION_CHUNK,
             BatchOperationChunkIntent.CREATE,
-            new BatchOperationCreateChunkProcessor(writers))
+            new BatchOperationCreateChunkProcessor(writers, keyGenerator))
         .onCommand(
             ValueType.BATCH_OPERATION_EXECUTION,
             BatchOperationExecutionIntent.EXECUTE,

nagyist/camunda-zeebe

(cherry picked from commit 1beac05a062f3bd130927941d7d3358830744885)

zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
@@ -128,7 +128,7 @@ public final class BatchOperationSetupProcessors {
         .onCommand(
             ValueType.BATCH_OPERATION_CHUNK,
             BatchOperationChunkIntent.CREATE,
-            new BatchOperationChunkCreateProcessor(writers))
+            new BatchOperationChunkCreateProcessor(writers, keyGenerator))
         .onCommand(
             ValueType.BATCH_OPERATION_EXECUTION,
             BatchOperationExecutionIntent.EXECUTE,