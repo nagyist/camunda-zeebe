329995d0bffa4de93c395ff7c14034bd2b5ab89a
ana.vinogradova <ana.vinogradova@camunda.com>
2026-02-11 10:04:13 +0000

    refactor: fixing linting

--- authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
+++ authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
@@ -62,7 +62,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.actuate.logging.LoggersEndpoint;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
