camunda/camunda

feat: implement RDBMS write operations for global listeners
The event types of a global listener are stored in a different table: in
order to update them they are simply removed and then re-added (same
approach as with canditate user/groups in user tasks).

db/rdbms/src/main/java/io/camunda/db/rdbms/sql/GlobalListenerMapper.java
@@ -1,0 +1,23 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.sql;
+
+import io.camunda.db.rdbms.write.domain.GlobalListenerDbModel;
+
+public interface GlobalListenerMapper {
+
+  void insert(GlobalListenerDbModel variable);
+
+  void insertEventTypes(GlobalListenerDbModel variable);
+
+  void update(GlobalListenerDbModel variable);
+
+  void delete(GlobalListenerDbModel variable);
+
+  void deleteEventTypes(GlobalListenerDbModel variable);
+}

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/GlobalListenerWriter.java
@@ -1,0 +1,81 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.write.service;
+
+import io.camunda.db.rdbms.write.domain.GlobalListenerDbModel;
+import io.camunda.db.rdbms.write.queue.ContextType;
+import io.camunda.db.rdbms.write.queue.ExecutionQueue;
+import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
+
+public class GlobalListenerWriter implements RdbmsWriter {
+
+  private final ExecutionQueue executionQueue;
+
+  public GlobalListenerWriter(final ExecutionQueue executionQueue) {
+    this.executionQueue = executionQueue;
+  }
+
+  public void create(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.INSERT,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.insert",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.INSERT,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.insertEventTypes",
+            globalListener));
+  }
+
+  public void update(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.UPDATE,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.update",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.deleteEventTypes",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.INSERT,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.insertEventTypes",
+            globalListener));
+  }
+
+  public void delete(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.deleteEventTypes",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.globalListenerKey(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.delete",
+            globalListener));
+  }
+}

nagyist/camunda-zeebe

db/rdbms/src/main/java/io/camunda/db/rdbms/sql/GlobalListenerMapper.java
@@ -1,0 +1,23 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.sql;
+
+import io.camunda.db.rdbms.write.domain.GlobalListenerDbModel;
+
+public interface GlobalListenerMapper {
+
+  void insert(GlobalListenerDbModel listener);
+
+  void insertEventTypes(GlobalListenerDbModel listener);
+
+  void update(GlobalListenerDbModel listener);
+
+  void delete(GlobalListenerDbModel listener);
+
+  void deleteEventTypes(GlobalListenerDbModel listener);
+}

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/GlobalListenerWriter.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.write.service;
+
+import io.camunda.db.rdbms.write.domain.GlobalListenerDbModel;
+import io.camunda.db.rdbms.write.queue.ContextType;
+import io.camunda.db.rdbms.write.queue.ExecutionQueue;
+import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
+
+public class GlobalListenerWriter implements RdbmsWriter {
+
+  private final ExecutionQueue executionQueue;
+
+  public GlobalListenerWriter(final ExecutionQueue executionQueue) {
+    this.executionQueue = executionQueue;
+  }
+
+  public void create(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.INSERT,
+            globalListener.id(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.insert",
+            globalListener));
+    if (globalListener.eventTypes() != null && !globalListener.eventTypes().isEmpty()) {
+      executionQueue.executeInQueue(
+          new QueueItem(
+              ContextType.GLOBAL_LISTENER,
+              WriteStatementType.INSERT,
+              globalListener.id(),
+              "io.camunda.db.rdbms.sql.GlobalListenerMapper.insertEventTypes",
+              globalListener));
+    }
+  }
+
+  public void update(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.UPDATE,
+            globalListener.id(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.update",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.id(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.deleteEventTypes",
+            globalListener));
+    if (globalListener.eventTypes() != null && !globalListener.eventTypes().isEmpty()) {
+      executionQueue.executeInQueue(
+          new QueueItem(
+              ContextType.GLOBAL_LISTENER,
+              WriteStatementType.INSERT,
+              globalListener.id(),
+              "io.camunda.db.rdbms.sql.GlobalListenerMapper.insertEventTypes",
+              globalListener));
+    }
+  }
+
+  public void delete(final GlobalListenerDbModel globalListener) {
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.id(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.deleteEventTypes",
+            globalListener));
+    executionQueue.executeInQueue(
+        new QueueItem(
+            ContextType.GLOBAL_LISTENER,
+            WriteStatementType.DELETE,
+            globalListener.id(),
+            "io.camunda.db.rdbms.sql.GlobalListenerMapper.delete",
+            globalListener));
+  }
+}